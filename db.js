5595d29be36408329a8c6a75739e16c590121177
Fatima Saleem <162432842+Fatimaaasaleem@users.noreply.github.com>
2026-07-10 13:53:28 +0500

    Update db.js

--- db.js
+++ db.js
@@ -25,37 +25,3 @@ pool.getConnection((err, conn) => {
 });
 
 module.exports = pool;
-
-
-On Fri, Jul 10, 2026 at 1:46 PM Samman Sajjad <sammansajjad156@gmail.com> wrote:
-@@ -1,19 +1,27 @@
-const mysql = require("mysql2");
-
-const connection = mysql.createConnection({
-const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 10000,
-});
-
-connection.connect((err) => {
-// Quick check on startup so we get a clear log message,
-// without holding a single long-lived connection open.
-pool.getConnection((err, conn) => {
-    if (err) {
-        console.log("Connection failed:", err);
-        return;
-    }
-    console.log("Connected to MySQL!");
-    conn.release();
-});
-
-module.exports = connection;
-module.exports = pool;
